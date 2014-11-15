2f6822a3a7c8e21e556dcec6fa072b1fb90454a8
Sudheendra Ayalasomayajula <errang@Sudheendras-MBP.home>
2014-11-14 21:30:10 -0500

    version -1

---
+++ BAGCA/questions/models.py
@@ -0,0 +1,26 @@
+import datetime
+
+from django.utils import timezone
+from django.db import models
+
+class Question(models.Model):
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    was_published_recently.admin_order_field='pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+class Choice(models.Model):
+    def __str__(self):
+        return self.choice_text
+
+    question = models.ForeignKey(Question)
+    choice_text = models.CharField(max_length=200)
+    choices = models.IntegerField(default=0)
