fa940b07ce1ffddfe8532792c37d913bb6ad061c
sudhee-udel <sudhee@udel.edu>
2014-12-04 22:37:41 -0500

    Allow user to email the choices made on the forms.

--- BAGCA_new/quizzes/urls.py
+++ BAGCA_new/quizzes/urls.py
@@ -3,7 +3,7 @@ from quizzes import views
 
 urlpatterns = patterns('',
                        url(r'^$', views.index, name='index'),
+                       url(r'^/results/', views.results, name='results'),
                        url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
-                       url(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote'),
-                       url(r'^(?P<question_id>\d+)/email/$', views.email, name='email')
+                       url(r'^(?P<question_id>\d+)/vote/$', views.vote, name='vote')
                        )
--- BAGCA_new/quizzes/views.py
+++ BAGCA_new/quizzes/views.py
@@ -1,12 +1,30 @@
 from django.http import HttpResponse, Http404
 from django.shortcuts import render
 from django.core.mail import send_mail
-
-from quizzes.models import Question
+from quizzes.models import Question, Choice
+import re
 
 def index(request):
+    if request.method == 'POST':
+        list = []
+        message = ''
+        for value in request.POST:
+            if 'question' in value:
+                list.append(value)
+
+         #message = message + "question_1 = " + request.POST.get('question_1', '') + "\n"
+
+        message = get_formatted_message(request.POST, list)
+
+        send_mail('Hello', message, 'chas.barnajr@tsgforce.com', ['sudhee1@gmail.com'], fail_silently=False)
+
+        return HttpResponse(str(message))
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
+    question_dictionary = {}
+    for q in latest_question_list:
+        question_dictionary[q] = q.choice_set.all()
+
+    context = {'question_dictionary': question_dictionary}
     return render(request, 'quizzes/index.html', context)
 
 def detail(request, question_id):
@@ -28,12 +46,25 @@ def vote(request, question_id):
     context = {'latest_question_list': latest_question_list}
     return render(request, 'quizzes/index.html', context)
 
-def email(request, question_id):
-    subject = 'Email subject'
-    message = 'Email message'
-    from_email = 'chas.barnajr@tsgforce.com'
-    to_email = 'chas.barnajr@tsgforce.com'
-    
-    send_mail(subject, message, from_email, [to_email], fail_silently=False)
+def results(request):
+    return HttpResponse("Hello.")
 
-    return render(request, 'quizzes/index.html')
+def get_formatted_message(post_data, list):
+    message = ''
+
+    for item in sorted(list):
+        question_re = re.match(r'question_(.*)', item, re.M)
+        message = message + "For question: " + str(Question.objects.get(pk=question_re.groups(1)[0])) + " you chose: " + str(Choice.objects.get(pk=post_data.get(item, ''))) + "\n"
+
+
+    return message
+
+def email(subject, message, from_addr, to_addr):
+    #subject = 'Email subject'
+    #message = 'Email message'
+    #from_email = 'chas.barnajr@tsgforce.com'
+    #to_email = 'chas.barnajr@tsgforce.com'
+
+    send_mail(subject, message, from_addr, to_addr, fail_silently=False)
+
+    return
