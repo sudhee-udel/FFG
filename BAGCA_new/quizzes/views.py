c14d5b6619cf5517ab55709c9575af42ee48f012
Chas Barna <cjb5333@gmail.com>
2015-01-16 13:13:22 -0500

    pages created, organized, and linked together

--- BAGCA_new/quizzes/views.py
+++ BAGCA_new/quizzes/views.py
@@ -15,48 +15,43 @@ import re
 
 @login_required
 def index(request):
-    if request.method == 'POST':
-        list = []
-        message = ''
+    trainings = Categories.objects.order_by('category_text')[:]
+    context = {'trainings': trainings}
+    return render(request, 'index.html', context)
 
-        for value in request.POST:
-            if 'question' in value:
-                list.append(value)
+@login_required
+def training(request, training_id):
+    try:
+        category = Categories.objects.get(pk=training_id)
+    except Categories.DoesNotExist:
+        raise Http404
 
-         #message = message + "question_1 = " + request.POST.get('question_1', '') + "\n"
+    training_info = {}
+    training_info['url'] = category.url
+    training_info['text']= category.category_text
+    training_info['id'] = training_id
 
-        message = get_formatted_message(request.POST, list)
+    context = {'training_info': training_info}
+    return render(request, 'trainings/training.html', context)
 
-#        send_mail('Hello', message, 'chas.barnajr@tsgforce.com', ['sudhee1@gmail.com'], fail_silently=False)
-        send_mail('Hello', message, 'chas.barnajr@tsgforce.com', [request.user.email], fail_silently=False)
+@login_required
+def quiz(request, training_id):
+    try:
+        latest_question_list = Question.objects.order_by('question_text')[:]
+    except Question.DoesNotExist:
+        raise Http404
 
-
-        return HttpResponse(str(message))
-    latest_question_list = Question.objects.order_by('question_text')[:]
     question_dictionary = {}
     for q in latest_question_list:
         question_dictionary[q] = q.choice_set.all()
 
-    category = {}
-    category['url'] = Categories.objects.get(pk=Categories._get_pk_val(latest_question_list[0].category)).url
+    return render(request, 'trainings/quiz.html', {'question_dictionary': question_dictionary})
 
-    context = {'question_dictionary': question_dictionary, 'category': category}
-    return render(request, 'quizzes/index.html', context)
+@login_required
+def results(request, training_id):
+    return render(request, 'quizzes/results.html', {})
 
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404
-
-    return render(request, 'quizzes/detail.html', {'question': question})
-
-def vote(request, question_id):
-    #return HttpResponse("You're voting on question %s." % question_id)
-    latest_question_list = Question.objects.order_by('question_text')[:]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'quizzes/index.html', context)
-
+# ***** this should probably go in a helper class
 def pdfs(request):
     # Create the HttpResponse object with the appropriate PDF headers.
     image = canvas.ImageReader('quizzes/BAGCA.jpg') # image_data is a raw string containing a JPEG
@@ -86,6 +81,7 @@ def pdfs(request):
     response.write(pdf)
     return response
 
+# ***** this should probably go in a helper class
 def get_formatted_message(post_data, list):
     message = ''
 
@@ -100,16 +96,26 @@ def get_formatted_message(post_data, list):
 
     return message
 
-def email(subject, message, from_addr, to_addr):
+# ***** this should probably go in a helper class
+# this method currently does not link to anything. will be used to send email.
+def email(request, subject, message, from_addr, to_addr):
     #subject = 'Email subject'
     #message = 'Email message'
     #from_email = 'chas.barnajr@tsgforce.com'
     #to_email = 'chas.barnajr@tsgforce.com'
+    if request.method == 'POST':
+        list = []
+        message = ''
+
+        for value in request.POST:
+            if 'question' in value:
+                list.append(value)
+
+        message = get_formatted_message(request.POST, list)
+
+#        send_mail('Hello', message, 'chas.barnajr@tsgforce.com', ['sudhee1@gmail.com'], fail_silently=False)
+        send_mail('Hello', message, 'chas.barnajr@tsgforce.com', [request.user.email], fail_silently=False)
+        return HttpResponse(str(message))
 
     send_mail(subject, message, from_addr, to_addr, fail_silently=False)
-
     return
-
-
-def training(request):
-    return render(request, 'quizzes/test.html', {})
