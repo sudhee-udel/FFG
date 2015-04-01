495315704aeb986ee9e2cd401829753ec8d45f27
sudhee-udel <sudhee@udel.edu>
2015-04-01 13:26:42 -0400

    - modified pluralize setting for credentials table.

--- BAGCA/credentials/models.py
+++ BAGCA/credentials/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 class Credentials(models.Model):
+    class Meta:
+        verbose_name_plural = "credentials"
+
     def __str__(self):
         return u'%s' % self.credential
 
